c86d745031f04b32c3f6fa1a4fd165eaf1e1003b
pradeepkumar181 <mailpradeepkr18@gmail.com>
2017-05-17 01:41:56 +0530

    navigation

---
+++ index.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+body {
+    margin: 0;
+}
+
+ul {
+    list-style-type: none;
+    margin: 0;
+    padding: 0;
+    width: 15%;
+    background-color: #4CAF50;
+    position: fixed;
+    height: 100%;
+    margin-top: -1.5%;
+    overflow: auto;
+    border-right: 1px solid #000000; 
+
+}
+
+li a {
+    display: block;
+    color: #fff ;
+    padding: 8px 16px;
+    text-decoration: none;
+}
+
+ul {
+    border-bottom: 1px solid #000000;
+}
+
+li {
+    text-align: center;
+    border-bottom: 1px solid #000;
+}
+
+
+li a:hover{
+    background-color: #fff;
+    color: black;
+}
+</style>
+</head>
+<body>
+
+<ul>
+  <li><a class="active" href="Q1.php">Question 1</a></li>
+  <li><a href="Q2.php">Question 2</a></li>
+  <li><a href="Q3.php">Question 3</a></li>
+</ul>
+</body>
+</html>
